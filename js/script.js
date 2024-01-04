2df9d9bd888393537d50dc58b770c42cd7d262c0
Matteo <125mamatucci@frhsd.com>
2024-01-04 09:49:11 -0500

    enter unfortunately doesn't work

--- js/script.js
+++ js/script.js
@@ -20,8 +20,61 @@ function intialize() {
       let tile = document.createElement("span");
       tile.id = r.toString() + "-" + c.toString();
       tile.classList.add("tile");
-      tile.innerText = "P";
+      tile.innerText = "";
       document.getElementById("board").appendChild(tile);
     }
-  } 
+  }
+  
+    // listen for key press
+     document.addEventListener("keyup", (e) => {
+        if (gameOver) return;
+
+       // alert(e.code);
+        if ("KeyA" <= e.code && e.code <=  "KeyZ"){
+            if (col < width){
+              let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+
+              if (currentTile.innerText == "") {
+                currentTile.innerText = e.code[3];
+                col++;
+              }
+            }
+        }
+        else if (e.code == "Backspace") {
+          if (0 < col && col <= width) {
+            col--;
+            let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+            currentTile.innerText = "";
+          }
+        }
+        else if (e.code == "Enter") {
+          update();
+          row += 1;
+          col = 0;
+        }
+
+        if (!gameOver && row == height) {
+          gameOver = true;
+          document.getElementById("answer").innerText = word;
+        }
+     })
+}
+
+function update() {
+  let correct = 0;
+  for (let c = 0; c < width; c++) {
+    let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+    let letter = currentTile.innerText;
+
+    if (word[c] == letter) {
+      currentTile.classList.add("correct");
+      correct += 1;
+    }
+    else if (word.includes(letter)) {
+      currentTile.classList.add("present");
+    }
+    else {
+      currentTile.classList.ass("absent");
+    }
+  }
 }
